3f547de4250a0a9e473a86c8201ab2acf4e75771
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-01-26 12:51:24 +0000

    Saving stock data to file to avoid API requests while messing with pandas

--- Putting_data_in_df.py
+++ Putting_data_in_df.py
@@ -3,7 +3,7 @@ test 2
 '''
 import pandas as pd
 import json as js
+import numpy as np
 
-from main import Stock_data_js_lst
-
-print(Stock_data_js_lst)
+Stock_data_js_lst = open('Stock_data.json')
+Stock_data_js_lst = js.load(Stock_data_js_lst)
