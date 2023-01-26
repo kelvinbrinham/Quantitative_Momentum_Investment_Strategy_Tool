a492cd2149f9004ed0d0694380d2e4222b07a842
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-01-26 13:29:47 +0000

    Update Putting_data_in_df.py

--- Putting_data_in_df.py
+++ Putting_data_in_df.py
@@ -17,7 +17,17 @@ Stock_data_js_lst = js.load(Stock_data_js_lst)
 #Mess with first dict of length 3 as in the API batch request list we will
 #Perform the following manipulation to add this to a dataframe using concat
 #By creating a df for each stock and then concat at the end
-Stock_data_js_lst = Stock_data_js_lst[0]
+Stock_data_js = Stock_data_js_lst[0]
+print(Stock_data_js)
+
+ticker_lst = ['MMM', 'AOS', 'ABT']
+ticker_string = ','.join(ticker_lst)
+
+#Put stock data into a df before analysis
+my_columns = ['Ticker', 'Price', 'One-Year Price Return', 'No. Shares to Buy'] #ADD MORE COLUMNS LATER
+
+Stock_df = pd.DataFrame(columns=my_columns)
+
 
 
 '''
@@ -26,8 +36,3 @@ print(Stock_data_js_lst['MMM'])
 print(Stock_data_js_lst['MMM']['stats'])
 print(Stock_data_js_lst['MMM']['stats']['marketcap'])
 '''
-
-#Put stock data into a df before analysis
-my_columns = ['Ticker', 'Price'] #ADD MORE COLUMNS LATER
-
-Stock_df = pd.DataFrame(columns=my_columns)
