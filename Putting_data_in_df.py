6ef2a8172262a746342ebac3dce1d6cce15c5cad
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-01-26 16:22:06 +0000

    Update Putting_data_in_df.py

--- Putting_data_in_df.py
+++ Putting_data_in_df.py
@@ -11,38 +11,13 @@ from scipy import stats
 import json as js
 import datetime as dt
 
-Stock_data_js_lst = open('Stock_data.json')
-Stock_data_js_lst = js.load(Stock_data_js_lst)
+API_key_file_path = r'/Users/kelvinbrinham/Documents/GitHub/Secret_Files/IEX_API_Key.txt'
+API_key = linecache.getline(API_key_file_path, 10).strip()
 
-#Mess with first dict of length 3 as in the API batch request list we will
-#Perform the following manipulation to add this to a dataframe using concat
-#By creating a df for each stock and then concat at the end
-Stock_data_js = Stock_data_js_lst[0]
-print(Stock_data_js)
+API_url = f'https://cloud.iexapis.com/beta/ref-data/symbols?token={API_key}'
 
-ticker_lst = ['MMM', 'AOS', 'ABT']
-ticker_string = ','.join(ticker_lst)
+tester = rq.get(API_url).json()
+print(tester)
 
-#Put stock data into a df before analysis
-my_columns = ['Ticker', 'Price'] #ADD MORE COLUMNS LATER
-
-df = pd.DataFrame(columns = my_columns)
-
-ticker = ticker_lst[0]
-ticker_df = pd.DataFrame([[ticker, Stock_data_js[ticker]['quote']['latestPrice']]], columns=my_columns)
-
-ticker = ticker_lst[1]
-ticker_df_2 = pd.DataFrame([[ticker, Stock_data_js[ticker]['quote']['latestPrice']]], columns=my_columns)
-
-df = pd.concat([ticker_df, ticker_df_2], axis = 0, ignore_index = True)
-
-print(df)
-
-
-
-'''
-print(Stock_data_js_lst)
-print(Stock_data_js_lst['MMM'])
-print(Stock_data_js_lst['MMM']['stats'])
-print(Stock_data_js_lst['MMM']['stats']['marketcap'])
-'''
+with open(f"list_of_tickers_supported.json", 'w') as f:
+    js.dump(tester, f)
