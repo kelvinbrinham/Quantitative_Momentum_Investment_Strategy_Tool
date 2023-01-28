30711de4ed92c4a49cdb62f8b07eb02117d3b85a
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-01-28 20:52:48 +0000

    cleaning

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ test
 
 
 from Momentum_Strategy_class import Momentum_strategy
-import config
 
-API_key = config.API_key
+from config import API_key
+
 
 my_strategy = Momentum_strategy(10000, 30, API_key)
 my_strategy.Order_sheet(0.5, 'S&P500_Stocks.csv', 'Symbol', 'OUTPUT/Order_sheet.xlsx', False)
