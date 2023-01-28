2684e210f3eea961bb983899af7c73671ee58fdf
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-01-28 20:40:47 +0000

    secret

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ test
 '''
 
 
-from Momentum_Strategy_class import Momentum_strategy
+# from Momentum_Strategy_class import Momentum_strategy
 
-my_strategy = Momentum_strategy(10000, 20)
-my_strategy.Order_sheet(0.5, 'S&P500_Stocks.csv', 'Symbol', 'OUTPUT/Order_sheet.xlsx', False)
+# my_strategy = Momentum_strategy(10000, 20)
+# my_strategy.Order_sheet(0.5, 'S&P500_Stocks.csv', 'Symbol', 'OUTPUT/Order_sheet.xlsx', False)
+
+import config
+
+print(config.KEY)
