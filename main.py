e243f4ca47548ab3e091a21aa77bddbd97c33f3c
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-01-28 20:56:04 +0000

    testing

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ from config import API_key
 
 
 my_strategy = Momentum_strategy(10000, 30, API_key)
-my_strategy.Order_sheet(0.5, 'S&P500_Stocks.csv', 'Symbol', 'OUTPUT/Order_sheet.xlsx', False)
+my_strategy.Order_sheet(0.5, 'S&P500_Stocks.csv', 'Symbol', '/Users/kelvinbrinham/Downloads/Quantitative_Momentum_Investment_Strategy_Tool-main/OUTPUT/bla.xlsx', False)
