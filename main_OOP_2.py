7ba763f146a73ec3e72e9b31f47969082d5e7638
Kelvin Brinham <kelvinbrinham@gmail.com>
2023-01-28 13:12:55 +0000

    User friendliness...

--- main_OOP_2.py
+++ main_OOP_2.py
@@ -38,7 +38,7 @@ class Momentum_strategy:
         self.__number_of_positions = number_of_positions
 
 
-    def Ticker_strings_lst_(self, filename: str):
+    def __Ticker_strings_lst_(self, filename: str):
         universe_df = pd.read_csv(filename)
 
         #Create List of stock tickers
@@ -49,8 +49,10 @@ class Momentum_strategy:
         for Ticker in Ticker_list:
             Ticker_list_stripped.append(Ticker.split()[0])
 
+        #CHANGE <><><><><><><><><><><><><><><><><><><><><><><><>
         #Shorten for testing to reduce API requests (slow and limited number of requests on free trial)
         Ticker_list_stripped = Ticker_list_stripped[:10]
+        #<><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
         #Create sub lists of tickers with length chunk_length so that each API batch request isn't too long
         chunk_length = 100
@@ -61,9 +63,9 @@ class Momentum_strategy:
         for i in range(len(Ticker_list_stripped_chunked)):
             Ticker_strings_lst.append(','.join(Ticker_list_stripped_chunked[i]))
 
-        return Ticker_strings_lst
+        return Ticker_strings_lst, Ticker_list_stripped_chunked
 
-    def batch_request(self):
+    def __batch_request(self, Ticker_strings_lst, Ticker_list_stripped_chunked):
         #Perform batch requests from API to retrieve data
         #Create list of dataframes (one for each stock)
         data_df_lst = []
@@ -72,12 +74,12 @@ class Momentum_strategy:
 
         #Loop over each ticker string
         for i in range(len(Ticker_list_stripped_chunked)):
-            API_url = f'https://cloud.iexapis.com/stable/stock/market/batch?symbols={Ticker_strings_lst[i]}&types=stats,quote,chart&token={API_key}'
+            API_url = f'https://cloud.iexapis.com/stable/stock/market/batch?symbols={Ticker_strings_lst[i]}&types=stats,quote,chart&token={Momentum_strategy.API_key}'
             #Request batch stock data
             Stock_data_js = rq.get(API_url).json()
             for ticker in Ticker_strings_lst[i].split(','):
                 #Write individual data frame for each stock and add to data_df_lst
-                if ticker in API_symbol_lst:
+                if ticker in Momentum_strategy.API_symbol_lst:
                     #Work out 1 year average Momentum
                     no_data_points = len(Stock_data_js[ticker]['chart']) # no. trading days in ytd
                     avg_ytd_mom = 0 # Average 1 day momentum ytd
@@ -104,18 +106,78 @@ class Momentum_strategy:
 
         return data_df
 
-    def analyse_momentum(self, Minimum_1d_momentum_hit_ratio):
-        Minimum_1d_momentum_hit_ratio = 0.1 #CHANGE THIS!!!!!!
-        data_df.drop(data_df[data_df['YTD 1-Day Momentum Hit Ratio'] < Minimum_1d_momentum_hit_ratio].index, inplace = True)
+    def __analyse_momentum(self, Minimum_1d_momentum_hit_ratio, dataframe):
+        # Minimum_1d_momentum_hit_ratio = 0.1 #CHANGE THIS!!!!!!
+        dataframe.drop(dataframe[dataframe['YTD 1-Day Momentum Hit Ratio'] < Minimum_1d_momentum_hit_ratio].index, inplace = True)
 
         #Sort the remaining stocks by YTD average 1-Day percentage momentum
-        data_df.sort_values('YTD Average 1-Day Percentage Momentum', ascending = False, inplace = True)
-        data_df.reset_index(inplace = True)
+        dataframe.sort_values('YTD Average 1-Day Percentage Momentum', ascending = False, inplace = True)
+        dataframe.reset_index(inplace = True)
 
-        return data_df
+        return dataframe
+
+    def __Create_output_spreadsheet(self, _filename, _dataframe):
+        # self.__investment = investment
+        # self.__number_of_positions = number_of_positions
+        # cash = 10000
+        # portfolio_length = 20
+
+        _dataframe = _dataframe[:self.__number_of_positions]
+
+        #Size of each EQUAL position
+        position_size = self.__investment / self.__number_of_positions
+
+        #Add number of stocks to buy to the spreadsheet
+        for i in range(len(_dataframe)):
+            _dataframe.loc[i, 'Buy'] = mth.floor(position_size / _dataframe['Price'][i])
+
+        #Drop unnecessary columns
+        _dataframe.drop(columns = ['level_0', 'index'], inplace = True)
+
+
+        #Formatting the Excel sheet
+        Momentum_strategy_file_name = _filename
+        _dataframe.to_excel(Momentum_strategy_file_name, sheet_name = 'Order_Sheet', startrow = 3, index = False)
+
+        #Load excel book
+        Momentum_strategy_wb = xl.load_workbook(Momentum_strategy_file_name)
+        Momentum_strategy_ws = Momentum_strategy_wb.active
+
+        #Format percentages
+        percentage_columns = ['C', 'D']
+        for letter in percentage_columns:
+            for i in range(5, 5 + self.__number_of_positions):
+                Momentum_strategy_ws[letter + str(i)].number_format = '0.00%'
+
+        #Add date and title
+        title_font = Font(name = 'Arial', size = 18, color = '000080', bold = True)
+        date_font = Font(name = 'Arial', size = 15)
+
+        Momentum_strategy_ws['A1'].font = title_font
+        Momentum_strategy_ws['A1'] = 'Momentum Trading Strategy'
+
+        Momentum_strategy_ws['A2'].font = date_font
+        Momentum_strategy_ws['A2'] = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        #Adjust column width to show contents clearly
+        for column_ in Momentum_strategy_ws.columns:
+            column_letter_ = column_[0].column_letter
+            width_ = max(len(cell.value) for cell in column_ if isinstance(cell.value, str))
+            Momentum_strategy_ws.column_dimensions[column_letter_].width = width_ + 3
+
+        #Save workbook
+        Momentum_strategy_wb.save(Momentum_strategy_file_name)
+
+
+    def Order_Sheet(self, Minimum_1d_momentum_hit_ratio, Index_filename__, Output_filename):
+        Ticker_strings_lst_, Ticker_list_stripped_chunked = self.__Ticker_strings_lst_(Index_filename__)
+        df = self.__batch_request(Ticker_strings_lst_, Ticker_list_stripped_chunked)
+        df = self.__analyse_momentum(Minimum_1d_momentum_hit_ratio, df)
+        self.__Create_output_spreadsheet(Output_filename, df)
 
 
 
 
-filename = 'S&P500_Stocks.csv'
-strategy = Momentum_strategy(10000, 30)
+
+# filename = 'S&P500_Stocks.csv'
+# strategy = Momentum_strategy(10000, 30)
--- test.py
+++ test.py
@@ -2,4 +2,9 @@
 test
 '''
 
-from main_OOP_2 import Equity
+
+from main_OOP_2 import Momentum_strategy
+
+strategy = Momentum_strategy(10000, 30)
+
+strategy.Order_Sheet(0.5, 'S&P500_Stocks.csv', 'Output.xlsx')
